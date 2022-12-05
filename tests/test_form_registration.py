70b55892c36f3bf2b1fb2938aece8777cd4c21b3
K1LLsounD <sgol99@mail.ru>
2022-12-05 23:32:42 +0300

    add delete comments

--- tests/test_form_registration.py
+++ tests/test_form_registration.py
@@ -8,14 +8,12 @@ browser.config.hold_browser_open = True
 def test_student_registration_form():
     browser.open('/automation-practice-form')
 
-    #user's characters
     TestFormLocators.UserCharacters.FIRSTNAME.type('Sergey')
     TestFormLocators.UserCharacters.LASTNAME.type('Golichenko')
     TestFormLocators.UserCharacters.EMAIL.type('example@.com')
     TestFormLocators.UserCharacters.GENDER_MEN.click()
     TestFormLocators.UserCharacters.NUMBER.type('880055355')
 
-    #birthday
     TestFormLocators.Birthday.BIRTH_INPUT.click()
     TestFormLocators.Birthday.BIRTH_MONTH.click()
     TestFormLocators.Birthday.BIRTH_MONTH_VALUE.click()
@@ -23,7 +21,6 @@ def test_student_registration_form():
     TestFormLocators.Birthday.BIRTH_YEAR_VALUE.click()
     TestFormLocators.Birthday.BIRTH_MONTH_DAY.click()
 
-    #hobbies
     TestFormLocators.Hobbies.SUBJECT.type('Maths').press_enter()
     TestFormLocators.Hobbies.HOBBIES_SPORTS.click()
     TestFormLocators.Hobbies.HOBBIES_MUSIC.click()
@@ -31,7 +28,6 @@ def test_student_registration_form():
     os.path.dirname(__file__), os.path.pardir, 'upload/AQA.png'
     )))
 
-    #adress
     TestFormLocators.Address.ADDRESS.type('улица Пушкина, Дом Колотушкина')
     TestFormLocators.Address.STATE.click()
     TestFormLocators.Address.STATE_VALUE.type('Haryana').press_enter()
